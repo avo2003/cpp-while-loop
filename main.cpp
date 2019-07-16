8fcaaede20365ce8156c68ab58d948af526b0f16
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-16 22:46:15 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int index=0;
+    while(index<300){
+
+         cout << index<< endl;
+           index++;
+    }
+
+    return 0;
+}
